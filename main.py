3b79ae8b85b7179e0e5da696e40827086e27f30b
Damianos93 <damianosmakris@hotmail.com>
2025-08-24 15:28:34 +0200

    Added the circle hitbox and Clock object

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_KINDS, ASTEROID_MAX_
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 1:
-        screen.fill("black")
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}!")
     print(f"Screen height: {SCREEN_HEIGHT}!")
