b5073cfee2da60376bb2c53ec7823f604c37fb60
Damianos93 <damianosmakris@hotmail.com>
2025-08-24 14:58:01 +0200

    Simple game loop. Black screen and quit button

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import pygame
-from constants import SCREEN_WIDTH,SCREEN_HEIGHT,ASTEROID_KINDS,ASTEROID_MAX_RADIUS,ASTEROID_MIN_RADIUS,ASTEROID_SPAWN_RATE
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE
+
+
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while 1:
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}!")
     print(f"Screen height: {SCREEN_HEIGHT}!")
